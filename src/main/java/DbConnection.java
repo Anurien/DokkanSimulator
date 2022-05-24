8fe1faee86e012a5def0c8a28b05edc45747fb4f
Nuria Garcia Cobas <ngarciacobas@danielcastelao.org>
2022-05-24 20:14:17 +0200

    Clase conexion a base de datos con SINGLETON

---
+++ src/main/java/DbConnection.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    /**
+     * Parametros de conexion
+     */
+    private static final String bd = "manual";
+    private static final String login = "root";
+    private static final String password = "Nuria21.";
+    private static final String url = "jdbc:mysql://localhost/" + bd;
+
+    static Connection connection = null;
+
+    /**
+     * Constructor de DbConnection
+     */
+    private DbConnection() {
+        try {
+            //obtenemos el driver de para mysql
+            Class.forName("com.mysql.jdbc.Driver");
+            //obtenemos la conexión
+            connection = DriverManager.getConnection(url, login, password);
+
+            if (connection != null) {
+                System.out.println("Conexión a base de datos " + bd + " OK\n");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Permite retornar la conexión
+     */
+    public static Connection getInstance() {
+        if (connection == null) new DbConnection();
+        return connection;
+    }
+
+    public static void desconectar() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
