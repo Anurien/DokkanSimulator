dcab086165f94c3c9f2584da8992bd2a55046afb
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-05-24 20:15:15 +0200

    Añado la clase Summonable y atributos de las cartas en la clase Summonable

---
+++ src/main/java/Summonable.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public abstract class Summonable {
+    protected String name;
+    protected int id;
+    protected ImageIcon icon;
+    protected Rarity rarity;
+    protected boolean rateUp;
+}
