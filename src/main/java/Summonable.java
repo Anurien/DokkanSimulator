5d8d95fbce487db4b2024248cdfa29757a3a3ee0
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-05-24 20:22:09 +0200

    Añado métodos equals y hashCode

--- src/main/java/Summonable.java
+++ src/main/java/Summonable.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Objects;
 
 public abstract class Summonable {
     protected String name;
@@ -25,5 +26,19 @@ public abstract class Summonable {
 
     public boolean isRateUp() {
         return rateUp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Summonable that = (Summonable) o;
+        return getId() == that.getId() &&
+                Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getId());
     }
 }
