12cf9fd6ee15cfe565e20c8c8d8e41ca21d89e46
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-05-25 11:00:23 +0200

    Añado los listeners de los botones con sus respectivas ventanas

--- src/main/java/MenuPrincipal.java
+++ src/main/java/MenuPrincipal.java
@@ -1,10 +1,16 @@
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 
 public class MenuPrincipal extends JFrame {
     private JButton help, summon1, score, sonido;
 
     JFrame frameMenu = new JFrame(" Dokkan Battle ");
+    JFrame frameAyuda = new JFrame("Ayuda");
+    JFrame frameHistorial = new JFrame("Historial");
     /*
      * Constructor
      */
@@ -15,19 +21,23 @@ public class MenuPrincipal extends JFrame {
          * */
         help = new JButton(new ImageIcon("src/resources/ayuda.png"));
         help.setBounds(7, 655, 51, 52);
+        help.addActionListener(new HelpButton());
 
 
 
         summon1 = new JButton(new ImageIcon());
         summon1.setBounds(130, 520, 159, 110);
+        summon1.addActionListener(new SummonListener());
 
 
         score = new JButton(new ImageIcon());
         score.setBounds(310, 510, 50, 50);
+        score.addActionListener(new ScoreButton());
 
 
         sonido = new JButton(new ImageIcon("src/resources/mute.png"));
         sonido.setBounds(12, 50, 51, 52);
+        sonido.addActionListener(new ButtonSonido());
 
 
         /*
@@ -66,4 +76,63 @@ public class MenuPrincipal extends JFrame {
         frameMenu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
+    private class ButtonSonido implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    private static class SummonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+
+
+        }
+    }
+
+
+    private class HelpButton implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+
+            JPanel panelMenu = new JPanel();
+            panelMenu.setOpaque(false);
+            panelMenu.setLayout(null);
+            panelMenu.setBounds(0, 0, 300, 580);
+
+            /*
+             * Dentro de la venatana de inicio de juego colocamos los botones
+             * a final de pagina.
+             * */
+            frameAyuda.add(panelMenu);
+            frameAyuda.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frameAyuda.setSize(400, 650);
+            frameAyuda.setResizable(false);
+            frameAyuda.setLocationRelativeTo(null);
+            frameAyuda.setVisible(true);
+        }
+    }
+
+
+
+    private class ScoreButton implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+
+
+            JPanel panelMenu = new JPanel();
+            panelMenu.setOpaque(false);
+            panelMenu.setLayout(null);
+            panelMenu.setBounds(0, 0, 435, 778);
+
+
+            frameHistorial.add(panelMenu);
+            frameHistorial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frameHistorial.setSize(435, 778);
+            frameHistorial.setResizable(false);
+            frameHistorial.setLocationRelativeTo(null);
+            frameHistorial.setVisible(true);
+        }
+    }
+
 }
