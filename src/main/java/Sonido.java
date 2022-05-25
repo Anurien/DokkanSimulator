4e723bf84f2cba2ce661699c2d96552628b2474f
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-05-25 11:10:37 +0200

    Añado clase Sonido con método de reproducirSonido

---
+++ src/main/java/Sonido.java
@@ -0,0 +1,17 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Sonido {
+
+    public static void reproducirSonido(String nombreSonido) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            System.out.println("Error al reproducir el sonido.");
+        }
+    }
+}
