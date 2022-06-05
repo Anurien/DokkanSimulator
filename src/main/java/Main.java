81668e14cea5a40d7ae252ae2aac861fb2c04b56
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-06-05 13:42:09 +0200

    Compruebo en el main que el proyecto funciona

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import Simulador.GachaInfoParser;
+import Simulador.MenuPrincipal;
+
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        GachaInfoParser parser = new GachaInfoParser("src/resources/GokuData.txt", "src/resources/FreezerData.txt");
+        parser.parse();
+        new MenuPrincipal();
+
+    }
 }
