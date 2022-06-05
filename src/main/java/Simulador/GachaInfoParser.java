130869297bc8aba641ce9842798494d197eed819
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-06-05 11:28:38 +0200

    close #23; Añado la clase para leer la información de las cartas en el juego y parsearlas

---
+++ src/main/java/Simulador/GachaInfoParser.java
@@ -0,0 +1,84 @@
+package Simulador;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class GachaInfoParser {
+    private Scanner GokuData;
+    private Scanner FreezerData;
+
+    public GachaInfoParser(String Goku, String Freezer) throws Exception {
+        GokuData = new Scanner(new File(Goku));
+        FreezerData = new Scanner(new File(Freezer));
+    }
+
+    public void parse() {
+        while (GokuData.hasNextLine()) {
+            String[] info = GokuData.nextLine().split("/");
+            String name = info[0];
+            int id = Integer.parseInt(info[2]);
+            boolean rateUp = (info[3].equals("Y"));
+            Rarity rarity;
+            switch (info[1]) {
+                case "SSR": {
+                    rarity = Rarity.SSR;
+                    CONSTANTES.goku_SSR.add(new GokuBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.goku_SSR_on_rateup = true;
+                    }
+                    break;
+                }
+                case "SR": {
+                    rarity = Rarity.SR;
+                    CONSTANTES.goku_SR.add(new GokuBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.goku_SR_on_rateup = true;
+                    }
+                    break;
+                }
+                case "R": {
+                    rarity = Rarity.R;
+                    CONSTANTES.goku_R.add(new GokuBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.goku_R_on_rateup = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        while (FreezerData.hasNextLine()) {
+            String[] info = FreezerData.nextLine().split("/");
+            String name = info[0];
+            int id = Integer.parseInt(info[2]);
+            boolean rateUp = (info[3].equals("Y"));
+            Rarity rarity;
+            switch (info[1]) {
+                case "SSR": {
+                    rarity = Rarity.SSR;
+                    CONSTANTES.freezer_SSR.add(new FreezerBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.freezer_SSR_on_rateup = true;
+                    }
+                    break;
+                }
+                case "SR": {
+                    rarity = Rarity.SR;
+                    CONSTANTES.freezer_SR.add(new FreezerBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.freezer_SR_on_rateup = true;
+                    }
+                    break;
+                }
+                case "R": {
+                    rarity = Rarity.R;
+                    CONSTANTES.freezer_R.add(new FreezerBanner(id, name, rarity, rateUp));
+                    if (rateUp) {
+                        CONSTANTES.freezer_R_on_rateup = true;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
